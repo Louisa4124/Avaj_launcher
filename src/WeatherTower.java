5a80682929872d8b19e7529669da2671b73956e9
Louisa <louisa.bjm@proton.me>
2026-01-22 15:28:47 +0100

    wip

--- src/WeatherTower.java
+++ src/WeatherTower.java
@@ -1,10 +1,9 @@
 public class WeatherTower extends Tower {
 
     public String getWeather(Coordinates p_coordinates) {
-        return WeatherProvider.getCurrentWeather(p_coordinates);
+        return WeatherProvider.getProvider().getCurrentWeather(p_coordinates);
     }
 
     public void changeWeather() {
-        
     }
 }
